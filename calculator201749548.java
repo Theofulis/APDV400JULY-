10faca7d9f2f6994aaac486ec98c2a35d0a37b7f
Theofulis’git config --global user.email ‘philusmashego@gmail.com <philusmashego@gmail.com>
2020-07-29 14:21:12 +0200

    calculator201749548

--- calculator201749548.java
+++ calculator201749548.java
@@ -5,10 +5,40 @@ public class calculator201749548 {
         
         double num1 = 8.0; 
         double num2 = 4.0;
-        //out of the integars insert into variables
-        System.out.println( " 8 + 4 = " + (num1 + num2));
-        System.out.println( " 8 - 4 = " + (num1 - num2));
-        System.out.println( " 8 * 4 = " + (num1 * num2));
-        System.out.println( " 8 / 4 = " + (num1 / num2));
+        
+    //out of the integars insert into variables
+    Addition(num1, num2);
+    Substratcion(num1, num2);
+    Multiplication(num1, num2);
+    Division(num1, num2);
+
+  }
+
+    static double Addition( double num1, double num2) {
+        System.out.println("Addition = " + (num1 + num2));
+        
+        return num1 + num2;
     }
+
+
+    static double Substratcion( double num1, double num2 ) {
+        System.out.println("Substratcion = " + (num1 - num2));
+        
+        return num1 - num2;
+    }
+
+
+    static double Multiplication( double num1, double num2 ) {
+        System.out.println("Multiplication = " + (num1 * num2));
+        
+        return num1 * num2;
+    }
+
+   static double Division( double num1, double num2 ) {
+        System.out.println("Divsion = " + (num1 / num2));
+       
+       return num1 / num2;
+    }
+                 
+                 
 }
