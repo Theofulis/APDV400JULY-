a8e67e6c88a3ef65645349560490df7ebe6cc6ae
Theofulis’git config --global user.email ‘philusmashego@gmail.com <philusmashego@gmail.com>
2020-07-29 12:35:56 +0200

    calculator201749548

---
+++ calculator201749548.java
@@ -0,0 +1,14 @@
+public class calculator201749548 {
+
+    //Simple Calculator 
+    public static void main(String[] args) {
+        
+        int num1 = 8; 
+        int num2 = 4;
+        //out of the integars insert into variables
+        System.out.println( " 8 + 4 = " + num1 + num2);
+        System.out.println( " 8 - 4 = " + (num1 - num2));
+        System.out.println( " 8 * 4 = " + (num1 * num2));
+        System.out.println( " 8 / 4 = " + (num1 / num2));
+    }
+}
